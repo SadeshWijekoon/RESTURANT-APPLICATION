5d2232dc204feaecd0fca96e4fe3470a2d25beca
SadeshWijekoon <10898706@students.plymouth.ac.uk>
2025-01-03 00:51:27 +0530

    Add server folder to repository

---
+++ server/__tests__/app.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const express = require('express');
+const app     = require('../index')
+
+// Test for the root route
+describe('GET /', () => {
+    it('should return "Server is running!"', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toBe('Server is running!');
+    });
+});
