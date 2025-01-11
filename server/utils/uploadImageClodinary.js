52339d87c83248e4c94e6a6719d1fb3d39db9077
SadeshWijekoon <10898706@students.plymouth.ac.uk>
2025-01-11 17:46:43 +0530

    add test file for utils folder

--- server/utils/uploadImageClodinary.js
+++ server/utils/uploadImageClodinary.js
@@ -1,21 +1,26 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-    cloud_name : process.env.CLODINARY_CLOUD_NAME,
-    api_key : process.env.CLODINARY_API_KEY,
-    api_secret : process.env.CLODINARY_API_SECRET_KEY
-})
+  cloud_name: process.env.CLODINARY_CLOUD_NAME,
+  api_key: process.env.CLODINARY_API_KEY,
+  api_secret: process.env.CLODINARY_API_SECRET_KEY,
+});
 
-const uploadImageClodinary = async(image)=>{
-    const buffer = image?.buffer || Buffer.from(await image.arrayBuffer())
+const uploadImageClodinary = async (image) => {
+  const buffer = image?.buffer || Buffer.from(await image.arrayBuffer());
 
-    const uploadImage = await new Promise((resolve,reject)=>{
-        cloudinary.uploader.upload_stream({ folder : "binkeyit"},(error,uploadResult)=>{
-            return resolve(uploadResult)
-        }).end(buffer)
-    })
+  const uploadImage = await new Promise((resolve, reject) => {
+    cloudinary.uploader
+      .upload_stream({ folder: 'binkeyit' }, (error, uploadResult) => {
+        if (error) {
+          return reject(error); // Reject the promise if there's an error
+        }
+        resolve(uploadResult); // Resolve the promise with the result
+      })
+      .end(buffer);
+  });
 
-    return uploadImage
-}
+  return uploadImage;
+};
 
-export default uploadImageClodinary
+export default uploadImageClodinary;
