bf6b39dd25c12e16448c847646c8547dc47c7b47
SadeshWijekoon <10898706@students.plymouth.ac.uk>
2025-01-12 23:23:17 +0530

    Update App component and tests

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
     <h1>hello world</h1>
+    <p>test</p>
       </>
   )
 }
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -4,8 +4,14 @@ import '@testing-library/jest-dom';
 import { test } from '@jest/globals';
 import App from './App';
 
-test('renders hello world', () => {
+test('renders hello world and test', () => {
   render(<App />);
+  
+  // Check if "hello world" is rendered
   const helloWorldElement = screen.getByText(/hello world/i);
   expect(helloWorldElement).toBeInTheDocument();
+
+  // Check if "test" is rendered
+  const testElement = screen.getByText(/test/i);
+  expect(testElement).toBeInTheDocument();
 });
